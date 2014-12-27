01d68c08fc3802eb13756971b20970c240917dd7
ilufang <cc2lufang@gmail.com>
2014-12-27 13:39:03 +0800

    Creation
    Empty Project

---
+++ MMDump/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MMDump
+//
+//  Created by ilufang on 12/27/14.
+//  Copyright (c) 2014 ilufang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
